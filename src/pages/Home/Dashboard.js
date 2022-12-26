bbbb23b1c2878f468de7f28af2a358410fbc38c3
danthecm <etchiedanie1@gmail.com>
2022-12-26 22:34:44 +0100

    feat: created running bot section with search, filter and new bot button

--- src/pages/Home/Dashboard.js
+++ src/pages/Home/Dashboard.js
@@ -4,6 +4,9 @@ import SideNav from "../../components/SideNav";
 import styles from "./Dashboard.module.css";
 
 import profile__avatar from "../../assets/Icons/avatar.svg";
+import search__icon from "../../assets/Icons/search.svg";
+
+import filter__icon from "../../assets/Icons/filter.svg";
 
 const Dashboard = () => {
   const user = JSON.parse(localStorage.getItem("user"));
@@ -27,7 +30,27 @@ const Dashboard = () => {
           </div>
           <h1>Welcome to TheMaxibot Forex</h1>
         </section>
-        
+        <section className={styles.main__content}>
+          <div className={styles.main__content__heading}>
+            <h2>Running Bots</h2>
+            <div className={styles.left__content}>
+              <div className={styles.search__field}>
+                <img
+                  className={styles.search__icon}
+                  src={search__icon}
+                  alt="search icon"
+                />
+                <input
+                  className={styles.search__input}
+                  type="text"
+                  placeholder="Search list"
+                />
+              </div>
+              <img src={filter__icon} alt="filter icon" />
+              <button className={styles.new__bot}>Add new</button>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
