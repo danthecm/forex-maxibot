820f08cda1d83884b66536a7d0566732c92d9a90
cm <etchiedaniel1@gmail.com>
2023-02-01 00:46:25 +0100

    fix: fix build errors with netlify caused by AuthProvider import in index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import routerConfig from "./router";
 import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
