a620f5b2a070137c3d293573ccee9851c716b599
danthecm <etchiedaniel1@gmail.com>
2023-02-04 22:28:38 +0100

    fix: trying to fix doulbe request error

--- src/hooks/use-axios-private.js
+++ src/hooks/use-axios-private.js
@@ -10,8 +10,9 @@ const useAxiosPrivate = () => {
   useEffect(() => {
     const requestInterceptor = axiosPrivate.interceptors.request.use(
       (config) => {
-        if (!config?.headers["Authorization"]) {
-            config.headers["Authorization"] = `Bearer ${auth?.accessToken}`;
+        if (!config.headers["Authorization"]) {
+          console.log("this is a normal request");
+          config.headers["Authorization"] = `Bearer ${auth?.accessToken}`;
         }
         return config;
       },
@@ -26,6 +27,7 @@ const useAxiosPrivate = () => {
         if (error?.response?.status === 401 && !prevRequest.sent) {
           prevRequest.sent = true;
           const newAccessToken = await refresh();
+          console.log("This is a try on prev request");
           prevRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
           return axiosPrivate(prevRequest);
         }
