356e970f031832338c300fa6b4db4f10fdbbb894
danthecm <etchiedaniel1@gmail.com>
2023-04-29 17:15:15 +0100

    feat: added profit margin and min_combo to edit bot modal

--- src/pages/Home/components/EditBot/index.js
+++ src/pages/Home/components/EditBot/index.js
@@ -36,12 +36,12 @@ const EditBot = ({ bot, close, setBots }) => {
   } = useInput((value) => value.trim() !== "", `${bot.take_profit}`);
 
   const {
-    value: enteredTradeClose,
-    inputBlurHandler: tradeCloseBlurHandler,
-    valueChangedHandler: tradeCloseChangedHandler,
-    isValid: tradeCloseIsValid,
-    hasError: tradeCloseHasError,
-  } = useInput((value) => value.trim() !== "", `${bot.close_trade}`);
+    value: enteredSkip,
+    inputBlurHandler: skipBlurHandler,
+    valueChangedHandler: skipChangedHandler,
+    isValid: skipIsValid,
+    hasError: skipHasError,
+  } = useInput((value) => value.trim() !== "", `${bot.skip}`);
 
   const {
     value: enteredStatus,
@@ -59,13 +59,40 @@ const EditBot = ({ bot, close, setBots }) => {
     hasError: pipMarginHasError,
   } = useInput((value) => value.trim() !== "", `${bot.pip_margin}`);
 
+  const {
+    value: enteredProfitMargin,
+    inputBlurHandler: profitMarginBlurHandler,
+    valueChangedHandler: profitMarginChangedHandler,
+    isValid: profitMarginIsValid,
+    hasError: profitMarginHasError,
+  } = useInput((value) => value.trim() !== "", `${bot.min_combo}`);
+
+  const {
+    value: enteredEquity,
+    inputBlurHandler: equityBlurHandler,
+    valueChangedHandler: equityChangedHandler,
+    isValid: equityIsValid,
+    hasError: equityHasError,
+  } = useInput((value) => value.trim() !== "", `${bot.equity}`);
+
+  const {
+    value: enteredMinCombo,
+    inputBlurHandler: minComboBlurHandler,
+    valueChangedHandler: minComboChangedHandler,
+    isValid: minComboIsValid,
+    hasError: minComboHasError,
+  } = useInput((value) => value.trim() !== "", `${bot.equity}`);
+
   const formIsValid =
     gridIntIsValid &&
     volumeIsValid &&
     tpIsValid &&
-    tradeCloseIsValid &&
+    skipIsValid &&
     pipMarginIsValid &&
-    statusIsValid;
+    statusIsValid &&
+    equityIsValid &&
+    profitMarginIsValid &&
+    minComboIsValid;
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
@@ -75,11 +102,14 @@ const EditBot = ({ bot, close, setBots }) => {
     const updating = toast.loading("Updating...");
     const botInfo = {
       grid_interval: enteredGridInt,
-      close_trade: enteredTradeClose,
+      skip: enteredSkip,
       pip_margin: enteredPipMargin,
       volume: enteredVolume,
       take_profit: enteredTP,
       status: enteredStatus,
+      equity: enteredEquity,
+      profit_margin: enteredProfitMargin,
+      min_combo: enteredMinCombo,
     };
 
     const updateBot = async () => {
@@ -112,84 +142,151 @@ const EditBot = ({ bot, close, setBots }) => {
   };
 
   return (
-    <StyledNewBot>
-      <Form onSubmit={formSubmitHandler}>
-        <label>Grid Interval:</label>
-        <Input
-          error={gridIntHasError}
-          onChange={gridIntChangedHandler}
-          onBlur={gridIntBlurHandler}
-          value={enteredGridInt}
-          type="number"
-          placeholder="Enter Grid Interval"
-        />
+    <StyledNewBot width="800px">
+      <Form layout="1fr 1fr" onSubmit={formSubmitHandler}>
+        <div>
+          <label>Grid Interval:</label>
+          <Input
+            error={gridIntHasError}
+            onChange={gridIntChangedHandler}
+            onBlur={gridIntBlurHandler}
+            value={enteredGridInt}
+            type="number"
+            placeholder="Enter Grid Interval"
+          />
+        </div>
         {gridIntHasError ? (
           <InputError>Grid Interval cannot be empty</InputError>
         ) : (
           ""
         )}
-        <label>Volume:</label>
-        <Input
-          error={volumeHasError}
-          onChange={volumeChangedHandler}
-          onBlur={volumeBlurHandler}
-          value={enteredVolume}
-          type="number"
-          placeholder="Enter Volume/Quantity"
-          step="0.01"
-        />
+        <div>
+          <label>Volume:</label>
+          <Input
+            error={volumeHasError}
+            onChange={volumeChangedHandler}
+            onBlur={volumeBlurHandler}
+            value={enteredVolume}
+            type="number"
+            placeholder="Enter Volume/Quantity"
+            step="0.01"
+          />
+        </div>
         {volumeHasError ? <InputError>Volume cannot be empty</InputError> : ""}
-        <label>Take Profit:</label>
-        <Input
-          error={tpHasError}
-          onChange={tpChangedHandler}
-          onBlur={tpBlurHandler}
-          value={enteredTP}
-          type="number"
-          placeholder="Enter Take Profit"
-          step="0.01"
-        />
+        <div>
+          <label>Take Profit:</label>
+          <Input
+            error={tpHasError}
+            onChange={tpChangedHandler}
+            onBlur={tpBlurHandler}
+            value={enteredTP}
+            type="number"
+            placeholder="Enter Take Profit"
+            step="0.01"
+          />
+        </div>
         {tpHasError ? <InputError>Take Profit cannot be empty</InputError> : ""}
-        <label>Close Margin:</label>
-        <Input
-          error={tradeCloseHasError}
-          onChange={tradeCloseChangedHandler}
-          onBlur={tradeCloseBlurHandler}
-          value={enteredTradeClose}
-          type="number"
-          placeholder="Enter Trade Close Margin"
-          step="0.01"
-        />
-        {tradeCloseHasError ? (
-          <InputError>Trade Close Margin cannot be empty</InputError>
-        ) : (
-          ""
-        )}
-        <label>Pip Margin:</label>
-        <Input
-          error={pipMarginHasError}
-          onChange={pipMarginChangedHandler}
-          onBlur={pipMarginBlurHandler}
-          value={enteredPipMargin}
-          type="number"
-          placeholder="Enter Pip Margin"
-          step="0.01"
-        />
-        {pipMarginHasError ? (
-          <InputError>Trade Close Margin cannot be empty</InputError>
-        ) : (
-          ""
-        )}
+        <div>
+          <label>Trade Skip:</label>
+          <Input
+            error={skipHasError}
+            onChange={skipChangedHandler}
+            onBlur={skipBlurHandler}
+            value={enteredSkip}
+            type="number"
+            placeholder="Enter Trade Close Margin"
+            step="0.01"
+          />
+        </div>
 
-        <label>Status</label>
-        <Input
-          error={statusHasError}
-          onChange={statusChangedHandler}
-          onBlur={statusBlurHandler}
-          value={enteredStatus}
-          type="text"
-          placeholder="Set Bot Status"
-        />
+        {skipHasError ? (
+          <InputError>Trade Skip cannot be empty</InputError>
+        ) : (
+          ""
+        )}
+        <div>
+          <label>Pip Margin:</label>
+          <Input
+            error={pipMarginHasError}
+            onChange={pipMarginChangedHandler}
+            onBlur={pipMarginBlurHandler}
+            value={enteredPipMargin}
+            type="number"
+            placeholder="Enter Pip Margin"
+            step="0.01"
+          />
+          {pipMarginHasError ? (
+            <InputError>Pip Margin cannot be empty</InputError>
+          ) : (
+            ""
+          )}
+        </div>
+
+        <div>
+          <label>Equity:</label>
+          <Input
+            error={equityHasError}
+            onChange={equityChangedHandler}
+            onBlur={equityBlurHandler}
+            value={enteredEquity}
+            type="number"
+            placeholder="Enter Equity"
+            step="0.001"
+          />
+          {pipMarginHasError ? (
+            <InputError>Trade Close Margin cannot be empty</InputError>
+          ) : (
+            ""
+          )}
+        </div>
+
+        <div>
+          <label>Profit Margin:</label>
+          <Input
+            error={profitMarginHasError}
+            onChange={profitMarginChangedHandler}
+            onBlur={profitMarginBlurHandler}
+            value={enteredProfitMargin}
+            type="number"
+            placeholder="Enter Pip Margin"
+            step="0.01"
+          />
+          {profitMarginHasError ? (
+            <InputError>Profit Margin cannot be empty</InputError>
+          ) : (
+            ""
+          )}
+        </div>
+
+        <div>
+          <label>Min Combo:</label>
+          <Input
+            error={minComboHasError}
+            onChange={minComboChangedHandler}
+            onBlur={minComboBlurHandler}
+            value={enteredMinCombo}
+            type="number"
+            placeholder="Enter Min Combo"
+            step="0.01"
+          />
+          {minComboHasError ? (
+            <InputError>Min Combo cannot be empty</InputError>
+          ) : (
+            ""
+          )}
+        </div>
+
+        <div>
+          <label>Status</label>
+          <Input
+            error={statusHasError}
+            onChange={statusChangedHandler}
+            onBlur={statusBlurHandler}
+            value={enteredStatus}
+            type="text"
+            placeholder="Set Bot Status"
+          />
+        </div>
         {statusHasError ? (
           <InputError>Trade Close Margin cannot be empty</InputError>
         ) : (
