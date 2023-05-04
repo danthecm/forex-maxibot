4314b7476dc06664cb06e8b0f65cbc9a0d62f605
danthecm <etchiedaniel1@gmail.com>
2023-05-04 16:43:52 +0100

    chore: rename index to url coz it contain just url

--- src/config/index.js
+++
@@ -1,2 +0,0 @@
-export const BASE_URL = "https://forex.themaxibot.com/"
-// export const BASE_URL = "http://127.0.0.1:8000/"
---
+++ src/config/urls.js
@@ -0,0 +1,4 @@
+export const BASE_URL = "https://api.forex.themaxibot.com/";
+export const REGISTER_URL = "register/";
+export const LOGIN_URL = "login/";
+// export const BASE_URL = "http://127.0.0.1:8000/"
