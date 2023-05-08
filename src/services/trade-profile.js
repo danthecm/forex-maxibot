73e2c4e02ab9a684d7d76ea426f7763e6b7228df
danthecm <etchiedaniel1@gmail.com>
2023-05-08 21:12:52 +0100

    feat: created trade-profile service

---
+++ src/services/trade-profile.js
@@ -0,0 +1,25 @@
+import { toast } from "react-toastify";
+import { NEW_PLATFORM_URL } from "../config/urls";
+
+export const newProfileReq = async (axiosPrivate, profileInfo) => {
+  const sending = toast.loading("Creating...");
+  try {
+    await axiosPrivate.post(NEW_PLATFORM_URL, profileInfo);
+    toast.update(sending, {
+      render: "Successfully Created Trade Profile",
+      type: "success",
+      isLoading: false,
+      autoClose: true,
+      closeButton: true,
+    });
+  } catch (e) {
+    toast.update(sending, {
+      render: "Error Creating Bot",
+      type: "error",
+      isLoading: false,
+      closeButton: true,
+      autoClose: true,
+    });
+    throw new Error("Error creating new trade profile");
+  }
+};
